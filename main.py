af157ad470811d4e6dfe347b57cae0418bda3b3b
Kid-Meelo <pierresatre1989@gmail.com>
2021-02-18 22:45:57 +0100

    Add login function

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+import getpass
+
 import requests
 from bs4 import BeautifulSoup
 
 
 # Access to website
-def open_url():
-    r = requests.get('https://www.onlineliga.de/login')
+def open_url(session: requests.Session):
+    # request in the session
+    r = session.get('https://www.onlineliga.de/login')
     soup = BeautifulSoup(r.text, "lxml")
     elements = soup.find_all(attrs={"name": "_token"})
     token = elements[0]['value']
-    print(token)
+    return token
+
+
+def login(session: requests.Session, token: str):
+    g = session.post("https://www.onlineliga.de/login",
+                     params={"_token": {token},
+                             "login": input("Whats your Username?"),
+                             "password": getpass.getpass("Whats your Password?")})
+
+
+def logintest(session: requests.Session):
+    x = session.get('https://www.onlineliga.de/office/finance')
+    print(x.text)
+    # if  x.text = finance then return true
+    # if x.text = login then return false
 
 
 if __name__ == '__main__':
-    open_url()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+    # open session
+    s = requests.Session()
+    t = open_url(s)
+    print(t)
+    login(s, t)
