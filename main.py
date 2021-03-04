6cc1d32ad3de7faf5f9ded50a3fd32f8a03c4032
Kid-Meelo <pierresatre1989@gmail.com>
2021-03-04 22:53:39 +0100

    Add login check and rename methods

--- main.py
+++ main.py
@@ -5,32 +5,42 @@ from bs4 import BeautifulSoup
 
 
 # Access to website
-def open_url(session: requests.Session):
+def get_ol_token(session: requests.Session):
     # request in the session
-    r = session.get('https://www.onlineliga.de/login')
-    soup = BeautifulSoup(r.text, "lxml")
+    response = session.get('https://www.onlineliga.de/login')
+    soup = BeautifulSoup(response.text, "lxml")
     elements = soup.find_all(attrs={"name": "_token"})
     token = elements[0]['value']
     return token
 
 
 def login(session: requests.Session, token: str):
-    g = session.post("https://www.onlineliga.de/login",
-                     params={"_token": {token},
-                             "login": input("Whats your Username?"),
-                             "password": getpass.getpass("Whats your Password?")})
+    login_response = session.post("https://www.onlineliga.de/login",
+                                  params={"_token": {token},
+                                          "login": input("Whats your Username?"),
+                                          "password": getpass.getpass("Whats your Password?")})
+    check_login(login_response)
 
 
-def logintest(session: requests.Session):
-    x = session.get('https://www.onlineliga.de/office/finance')
-    print(x.text)
+def check_login(response: requests.Response):
+    # if not response.text:
+    #     print("login successful")
+    if len(response.text) == 0:
+        print("login successful")
+    else:
+        print("login unsuccessful")
+
+
+# def logintest(session: requests.Session):
+#     x = session.get('https://www.onlineliga.de/office/finance')
+#     print(x.text)
     # if  x.text = finance then return true
     # if x.text = login then return false
 
 
 if __name__ == '__main__':
     # open session
-    s = requests.Session()
-    t = open_url(s)
-    print(t)
-    login(s, t)
+    ol_session = requests.Session()
+    ol_token = get_ol_token(ol_session)
+    print(ol_token)
+    login(ol_session, ol_token)
